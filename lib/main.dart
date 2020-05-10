2e2f9c365b7ae8467c0f474a8dc9068df3cd0ffb
tanmaythag <tanmaypthag30@gmail.com>
2020-05-10 13:15:27 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+
+      home: Scaffold(
+
+        backgroundColor: Colors.teal,
+        body:SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  //backgroundColor: Colors.red,
+                  backgroundImage: AssetImage('images/me.jpg'),
+                ),
+                Text('Tanmay Thag',
+                  style:TextStyle(
+                     fontFamily:'Architects Daughter',
+                     fontSize:40.0,
+                     color: Colors.white,
+                     //fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'APPLICATION DEVELOPER',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+
+                ),
+                SizedBox(
+                  height:20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade900,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading:Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        '+918956063010',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    )
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading:Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'tanmaypthag@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                      ),
+                    ),
+
+                  )),
+                Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading:Icon(
+                        Icons.link,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        'https://linkedin.com/in/tanmay-thag-299927171',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 20.0,
+                        ),
+                      ),
+
+                    )),
+
+
+
+
+
+              ],
+            )),
+      ),
+    );
+  }
+}
+
+
